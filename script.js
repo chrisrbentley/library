2539c3cfddae4deb43e38378a14b74424340bb87
Chris Bentley <chrisrbentley@pm.me>
2022-06-04 17:20:31 -0400

    Updated status input.

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
 const container = document.getElementById("container");
 const form = document.getElementById("form");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const read = document.getElementById("read");
 const submitBtn = document.getElementById("submit");
 const books = document.getElementById("books");
 
 let myLibrary = []
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        let info = this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read;
-        return info;
-    }
-}
-
-
-const template = new Book('Book Title', 'Book Author', 295, 'true');
-console.log(template.info());
-
-const wimpyKid = new Book('Wimpy Kid', 'jeff', 101, 'true');
-
-
 function addBookToLibrary() {
-    myLibrary.push(template, wimpyKid);
+    submitBtn.addEventListener('click', function(event){
+        event.preventDefault();
+        let book = {
+            title: document.getElementById("title").value,
+            author: document.getElementById("author").value,
+            pages: document.getElementById("pages").value,
+            status: document.getElementById("status").value,
+            info: function() {
+                let info = this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.status;
+            return info;
+            }
+        }
+        myLibrary.push(book);
+        document.forms[0].reset()
+    })
 }
 addBookToLibrary();
 
 function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        console.log('test');
-        const card = document.createElement("div");
-        card.setAttribute('class', 'book-card')
-        books.appendChild(card);
-        card.textContent = myLibrary[i].info();
-    }
+    submitBtn.addEventListener('click', () => {
+        for (let i = 0; i < myLibrary.length; i++) {
+            console.log('test');
+            const card = document.createElement("div");
+            card.setAttribute('class', 'book-card')
+            books.appendChild(card);
+            card.textContent = myLibrary[i].info();
+        }
+    })
 }
 displayBooks();
 
@@ -48,6 +42,7 @@ const newBook = document.createElement("button");
 newBook.setAttribute("id", "new-book-btn");
 newBook.textContent = 'Add Book';
 container.appendChild(newBook);
+
 
 form.style.display = 'none';
 function showForm() {
@@ -58,4 +53,3 @@ function showForm() {
     })
 }
 showForm();
-
